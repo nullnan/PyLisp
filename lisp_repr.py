434ca7112f60d5a72c336cf1952a33d0cfbd1241
Null NaN <nullnan@users.noreply.github.com>
2023-03-22 01:25:23 +0800

    feat: improve the REPL.

--- lisp_repr.py
+++ lisp_repr.py
@@ -2,12 +2,14 @@ from evaluator import Evaluator
 from parser import Parser
 
 if __name__ == '__main__':
-    print("""
-Welcome to PyLisp REPL v0.0.1
-    """)
+    print("""Welcome to PyLisp REPL v0.0.1""")
     env = {}
     while True:
         source_code = input('> ')
-        root = Parser(source_code).parse()
-        result = Evaluator.eval(root, env)
-        print(str(result))
+        try:
+            root = Parser(source_code).parse()
+            result = Evaluator.eval(root, env)
+            if result is not None:
+                print(result)
+        except Exception as e:
+            print(f'{type(e).__name__}:\n\t{e}')
